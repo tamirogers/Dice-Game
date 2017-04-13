e14215c6b978478f6eda42ebeb9e9c668971bfd7
Tami R <tami1989@yahoo.com>
2017-04-12 23:56:21 -0700

    game code done

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ var scores, roundScore, activePlayer, gameplaying;
 
 init();
 
+var lastDice;
+
 
 //click event triggers anonymous function w/no name
 document.querySelector('.btn-roll').addEventListener('click', function() {
@@ -20,16 +22,25 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     if (gamePlaying) {
         //need random number
         var dice = Math.floor(Math.random() * 6) + 1;
+        // var dice2 = Math.floor(Math.random() * 6) + 1;
 
         //display result
+
         var diceDOM = document.querySelector('.dice');
         diceDOM.style.display = 'block';
+
         //concats the name in html w/the number picked in dice variable
         diceDOM.src = 'dice-' + dice + '.png';
+        // rolls two 6s
+        if (dice === 6 && lastDice === 6) {
+            //lose score
+            scores[activePlayer] = 0;
+            document.querySelector('#score-' + activePlayer).textContent = '0';
+            nextPlayer();
 
-        //Update roundScore if 1 was not rolled
-        if (dice !== 1) {
-            //add score, same as roundScore = roundScore + dice
+            //Update roundScore if 1 was not rolled
+        } else if (dice !== 1) {
+            //add score, same as roundScore = roundScore + dice.  .textContent is a method
             roundScore += dice;
             document.querySelector('#current-' + activePlayer).textContent = roundScore;
         } else {
@@ -37,9 +48,9 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
             nextPlayer();
 
         }
+        // the value of dice is stored in new var 
+        lastDice = dice;
     }
-
-
 });
 
 //hold button
@@ -50,8 +61,19 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
         //update UI
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
-        //Check who won	
-        if (scores[activePlayer] >= 20) {
+        var input = document.querySelector('.final-score').value;
+        var winningScore;
+
+        // if input is undefined, 0, null, or empty string " ", automatically coerced to FALSE
+        // this says if input is true
+        if (input) {
+            var winningScore = input;
+        } else {
+            winningScore = 20;
+        }
+
+        //Check who won 
+        if (scores[activePlayer] >= winningScore) {
             document.querySelector('#name-' + activePlayer).textContent = "Winner!";
             document.querySelector('.dice').style.display = 'none';
             //add the css class already created
@@ -86,6 +108,7 @@ function nextPlayer() {
     document.querySelector('.player-1-panel').classList.toggle('active');
     //makes dice disapear when rolls 1
     document.querySelector('.dice').style.display = 'none';
+    // document.querySelector('.dice2').style.display = 'none';
 }
 document.querySelector('.btn-new').addEventListener('click', function() {
     init();
@@ -98,6 +121,7 @@ function init() {
     activePlayer = 0;
     gamePlaying = true;
     document.querySelector('.dice').style.display = 'none';
+    // document.querySelector('.dice2').style.display = 'none';
 
     //Sets all the numbers to 0, getElementById is faster than query (does same thing)
     document.getElementById('score-0').textContent = '0';
@@ -114,7 +138,3 @@ function init() {
 }
 
 
-
-//query selects to the html
-
-//textContent is a method
